8307a9256fcfa10070095b28366fd1f449bfe643
Master Didoo <didoo77@mail.ru>
2013-05-17 11:49:47 +0400

    test intersect

--- js/editors/editor-area.js
+++ js/editors/editor-area.js
@@ -2,8 +2,9 @@ var App = App || {};
 
 (function ($) {
 
-    function Sector(s, place, scale) {
+    function Sector(s, place, a, scale) {
         this.size = 10;
+        this.area = a;
         this.scale = scale == null ? 1 : scale;
         this.isTitleVisible = true;
         this.init(place);
@@ -21,22 +22,44 @@ var App = App || {};
             this._title = $('<div class="ea-sector-title">').appendTo(this._dragHandler);
             this.jq.draggable({
                 containment: 'parent',
+//                revert: 'invalid',
+//                zIndex: 10,
                 handle: this._dragHandler,
-                stop: function () {
-                    self._updatePos();
+                drag: function (event, info) {
+                    self._updatePos(info);
                 }
             });
             this._rotator.rotatable({
                 stop: function (a) {
-                    self.rotate = a;
+                    var polygon = self.polygon.clone();
+//                    self.polygon.rotate();
+                    self.polygon.rotate(a - self.rotate);
+                    if (self.area.isIntersect(self.polygon)) {
+                        self.polygon = polygon;
+                        self._rotator.rotate(self.rotate);
+                    } else {
+                        self.rotate = a;
+
+                        //TODO debug
+                        self.debug = Debug.debugPoly(self.polygon, self.debug);
+                    }
                 }
             });
         },
-        _updatePos: function () {
+        _updatePos: function (info) {
             if (this.jq != null) {
-                var from = this.jq.position();
-                this.top = Math.round(from.top / this.scale);
-                this.left = Math.round(from.left / this.scale);
+                var from = info.position,
+                    newTop = Math.round(from.top / this.scale),
+                    newLeft = Math.round(from.left / this.scale);
+                this.polygon.moveTo(newLeft, newTop);
+                if (this.area.isIntersect(this.polygon)) {
+                    this.polygon.moveTo(this.left, this.top);
+                    from.top = this.top * this.scale;
+                    from.left = this.left * this.scale;
+                    return;
+                }
+                this.top = newTop;
+                this.left = newLeft;
             }
         },
         setScale: function (scale) {
@@ -112,7 +135,7 @@ var App = App || {};
             }
             this.top = data.top;
             this.left = data.left;
-            this.rotate = data.rotate;
+            this.rotate = data.rotate == null ? 0 : data.rotate;
             this.name = data.name;
             this.placeRows = data.placeRows;
             //TODO вынести в код сохранения сектора
@@ -141,15 +164,23 @@ var App = App || {};
             this._width = placePos.max - placePos.min + 1;
             this._height = rowPos.max - rowPos.min + 1;
             this.places = [];
+            this.polygon = new Polygon(this._width * this.size / 2, this._height * this.size / 2);
             var self = this;
             data.placeRows.forEach(function (r) {
                 var row = parseInt(r.pos) - rowPos.min;
                 if (r.places instanceof Array) {
                     r.places.forEach(function (p) {
-                        self.places.push([row, parseInt(p.pos) - placePos.min, r.shift]);
+                        var col = parseInt(p.pos) - placePos.min;
+                        self.places.push([row, col, r.shift]);
+                        self.polygon.addPlace(col, row, self.size, r.shift);
                     });
                 }
             });
+            if (this.rotate != null) {
+                self.polygon.rotate(this.rotate);
+            }
+            self.debug = Debug.debugPoly(this.polygon);
+            self.polygon.moveTo(this.left, this.top);
             this.render();
         },
         json: function () {
@@ -163,43 +194,230 @@ var App = App || {};
         }
     }
 
+    var Debug = {
+        isDisabled: true,
+        debug: function (p, m) {
+            if (this.isDisabled) return;
+            var out = '';
+            p.forEach(function (polygon) {
+                out += '\n';
+                polygon.forEach(function (s) {
+                    out += '[' + s.X + ',' + s.Y + '] '
+                })
+            })
+            console.debug(out + ' : ' + m);
+        },
+
+        // converts polygons to SVG path string
+        polys2path: function (poly, scale) {
+            if (this.isDisabled) return;
+            var path = "", i, j;
+            if (!scale) scale = 1;
+            for (i = 0; i < poly.length; i++) {
+                for (j = 0; j < poly[i].length; j++) {
+                    if (!j) path += "M";
+                    else path += "L";
+                    path += (poly[i][j].X / scale) + ", " + (poly[i][j].Y / scale);
+                }
+                path += "Z";
+            }
+            return path;
+        },
+
+        debugPoly: function (p, d) {
+            if (this.isDisabled) return;
+            var polygon = p.clone();
+            polygon.moveTo(160 / 2, 160 / 2);
+            var svg = '<svg style="margin-top:10px; margin-right:10px;margin-bottom:10px;background-color:#dddddd;" width="160" height="160">'
+                + '<path stroke="black" fill="green" stroke-width="0" d="' + Debug.polys2path(polygon.polygons, 1) + '"/>'
+                + '</svg>';
+            var div = d;
+            if (div == null) {
+                div = $('<div style="float: left"></div>');
+                $('#svg').append(div);
+            }
+            div.html(svg);
+            return div;
+        },
+
+        debugArea: function (a) {
+            if (this.isDisabled) return;
+            var paths = '';
+            a.intersects.forEach(function (p) {
+                paths += '<path stroke="black" fill="green" stroke-width="0" d="' + Debug.polys2path(p.polygon.polygons, 1) + '"/>'
+
+            });
+            var svg = '<svg style="margin-top:10px; margin-right:10px;margin-bottom:10px;background-color:#dddddd;" width="1280" height="600">'
+                + paths
+                + '</svg>';
+            $('#svgarea').html(svg);
+        }
+    }
+
+    function Polygon(x, y) {
+        this.polygons = null;
+        this.X = x;
+        this.Y = y;
+    }
+
+    Polygon.prototype = {
+        clone: function () {
+            var n = new Polygon(this.X, this.Y);
+            n.polygons = [];
+            this.polygons.forEach(function (polygon) {
+                var p = [];
+                polygon.forEach(function (point) {
+                    p.push({X: point.X, Y: point.Y});
+                });
+                n.polygons.push(p);
+            });
+            return n;
+        },
+        isIntersect: function (p) {
+            var cpr = new ClipperLib.Clipper();
+            cpr.AddPolygons(this.polygons, ClipperLib.PolyType.ptSubject);
+            cpr.AddPolygons(p.polygons, ClipperLib.PolyType.ptClip);
+            var rez = [
+                []
+            ];
+            cpr.Execute(ClipperLib.ClipType.ctIntersection, rez, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
+            return rez.length > 0;
+        },
+        moveTo: function (x0, y0) {
+            var x = x0 - this.X,
+                y = y0 - this.Y;
+            this.points().forEach(function (p) {
+                p.X += x;
+                p.Y += y;
+            });
+            this.X = x0;
+            this.Y = y0;
+        },
+        setCenter: function (x, y) {
+            this.X = x + 1;
+            this.Y = y;
+        },
+        rotate: function (angle) {
+            var x0 = this.X,
+                y0 = this.Y,
+                a = angle / 180 * Math.PI,
+                cos = Math.cos(a),
+                sin = Math.sin(a);
+            this.points().forEach(function (node) {
+                var x = x0 + (node.X - x0) * cos + (y0 - node.Y) * sin;
+                var y = y0 + (node.X - x0) * sin + (node.Y - y0) * cos;
+                node.X = x;
+                node.Y = y;
+            });
+        },
+        points: function () {
+            var points = [];
+            this.polygons.forEach(function (polygon) {
+                polygon.forEach(function (point) {
+                    points.push(point);
+                });
+            });
+            return points;
+        },
+        setRectangle: function (x, y, w, h) {
+            this.polygons = [
+                [
+                    {X: x, Y: y},
+                    {X: x + w + 1, Y: y},
+                    {X: x + w + 1, Y: y + h + 1},
+                    {X: x, Y: y + h + 1}
+                ]
+            ];
+        },
+        addPlace: function (col, row, size, shift) {
+            var x = col * size,
+                y = row * size;
+            if (shift > 0) {
+                x += size / 2;
+            } else if (shift < 0) {
+                x -= size / 2;
+            }
+            var newP = [
+                [
+                    {X: x, Y: y},
+                    {X: x + size, Y: y},
+                    {X: x + size, Y: y + size },
+                    {X: x, Y: y + size }
+                ]
+            ];
+            if (this.polygons == null) {
+                this.polygons = newP;
+                return;
+            }
+            var rez = [
+                []
+            ];
+            var cpr = new ClipperLib.Clipper();
+            cpr.AddPolygons(this.polygons, ClipperLib.PolyType.ptSubject);
+            cpr.AddPolygons(newP, ClipperLib.PolyType.ptClip);
+            cpr.Execute(ClipperLib.ClipType.ctUnion, rez, ClipperLib.PolyFillType.pftNonZero, ClipperLib.PolyFillType.pftNonZero);
+            this.polygons = rez;
+        }
+    }
+
     function Area(a, place) {
         var self = this;
         this.id = a.id;
         this.name = a.name;
         this.sectors = [];
+        this.intersects = [];
         this.isSectorTtitleVisible = true;
         this.scale = 1;
         this.top = a.top;
         this.left = a.left;
         this.height = a.height;
         this.width = a.width;
-        this.scene = $('<div class="ae-area-scene" title="Сцена"><strong>Сцена</strong></div>').css({
-            position: 'absolute',
-            top: a.top,
-            left: a.left,
-            height: a.height,
-            width: a.width
-        }).resizable({
-                autoHide: true,
-                stop: function () {
-                    self._updatePos();
-                }
-            }).draggable({
-                containment: 'parent',
-                stop: function () {
-                    self._updatePos();
-                }
-            });
+
         a.sectors.forEach(function (s) {
-            var sector = new Sector(s, place);
-            self.sectors.push(sector);
+            var sector = new Sector(s, place, self);
+            self.addSector(sector);
         });
     }
 
     Area.prototype = {
+        addSector: function (sector) {
+            this.sectors.push(sector);
+            this.intersects.push(sector);
+        },
+        isIntersect: function (polygon) {
+            Debug.debugArea(this);
+            var polygons = this.intersects;
+            for (var i = 0; polygons.length > i; i++) {
+                if (polygons[i].polygon != polygon) {
+                    if (polygon.isIntersect(polygons[i].polygon)) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        },
         appendScene: function (place) {
-            this.scene.appendTo(place);
+            var self = this;
+            this.scenePolygon = new Polygon(0, 0);
+            this.intersects.push({polygon: this.scenePolygon});
+            this.scenePolygon.setRectangle(this.left, this.top, this.width, this.height);
+            this.scene = $('<div class="ae-area-scene" title="Сцена"><strong>Сцена</strong></div>').appendTo(place);
+            this.scene.css({
+                top: this.top,
+                left: this.left,
+                height: this.height,
+                width: this.width
+            }).resizable({
+                    autoHide: true,
+                    stop: function () {
+                        self._updateSize();
+                    }
+                }).draggable({
+                    containment: 'parent',
+                    drag: function (e,info) {
+                        self._updatePos(info);
+                    }
+                });
         },
         setScale: function (scale) {
             this.sectors.forEach(function (s) {
@@ -213,12 +431,34 @@ var App = App || {};
             });
             this.scale = scale;
         },
-        _updatePos: function () {
-            var from = this.scene.position();
-            this.top = Math.round(from.top / this.scale);
-            this.left = Math.round(from.left / this.scale);
-            this.height = Math.round(this.scene.height() / this.scale);
-            this.width = Math.round(this.scene.width() / this.scale);
+        _updatePos: function (info) {
+            var from = info.position,
+                top = Math.round(from.top / this.scale),
+                left = Math.round(from.left / this.scale);
+            this.scenePolygon.setRectangle(left, top, this.width, this.height);
+            if (this.isIntersect(this.scenePolygon)) {
+                this.scenePolygon.setRectangle(this.left, this.top, this.width, this.height);
+                from.top = this.top * this.scale;
+                from.left = this.left * this.scale;
+            } else {
+                this.top = top;
+                this.left = left;
+            }
+        },
+        _updateSize: function () {
+            var height = Math.round(this.scene.height() / this.scale),
+                width = Math.round(this.scene.width() / this.scale);
+            this.scenePolygon.setRectangle(this.left, this.top, width, height);
+            if (this.isIntersect(this.scenePolygon)) {
+                this.scenePolygon.setRectangle(this.left, this.top, this.width, this.height);
+                this.scene.css({
+                    height: this.height * this.scale,
+                    width: this.width * this.scale
+                });
+            } else {
+                this.height = height;
+                this.width = width;
+            }
         },
         json: function () {
             var sectors = [];
@@ -256,6 +496,7 @@ var App = App || {};
 
     var Editor = {
         scale: 1,
+        isVisibleTitle: true,
         init: function (place) {
             this.place = place;
             this.list = place.find('#ae-sectors-list');
@@ -266,7 +507,7 @@ var App = App || {};
             });
             var tmp = place.find("#ae-area-view");
             tmp.resizable();
-            tmp.height(window.innerHeight - 40);
+            tmp.height(window.innerHeight - 600);
             var view = place.find('#ae-sectors-holder');
             this.view = view;
             this.height = view.height();
@@ -275,9 +516,9 @@ var App = App || {};
                 $('#ae-rez').text(Editor.json());
             });
             place.find('#ae-show-sector-title').on('change', function (e) {
-                var checked = this.checked;
+                Editor.isVisibleTitle = this.checked;
                 Editor.area.sectors.forEach(function (s) {
-                    s.setVisibleTitle(checked);
+                    s.setVisibleTitle(Editor.isVisibleTitle);
                 });
             });
             place.find('#ae-zoom-in').on('click', function (e) {
@@ -338,9 +579,10 @@ var App = App || {};
         _newSector: function (json) {
             Editor.place.show();
             if (json != null) {
-                Editor.editSector = new Sector(json, Editor.view, Editor.scale);
+                Editor.editSector = new Sector(json, Editor.view, Editor.area, Editor.scale);
+                Editor.editSector.setVisibleTitle(Editor.isVisibleTitle);
                 Editor._addSector(Editor.editSector);
-                Editor.area.sectors.push(Editor.editSector); //TODO изменить
+                Editor.area.addSector(Editor.editSector); //TODO изменить
             }
         }
     }
